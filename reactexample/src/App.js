50a839510c414d3d9ec1699d1b537302d13cea9e
Yagnik Mayani <yagnikmayani15@gmail.com>
2025-02-18 08:02:11 -0800

    Add Files

---
+++ reactexample/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import '../src/input.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='btn'>Hi my name</h1>
+    </>
+  );
+}
+
+export default App;
